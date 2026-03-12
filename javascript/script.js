d05d69dbd5223bc7addf2f4ee7bcb4806a08c016
iamshashankyadav <shashanky@iitbhilai.ac.in>
2026-03-12 20:22:12 +0530

    fn

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
-let arr=[1,43,"42","234",true,false]
-for (let i of arr){
-console.log(i,typeof(i))}
+let arr=[250,650,300,900,50]
+for (let i=0 ; i<arr.length;i++){
+    arr[i]=arr[i]*0.9
+console.log(arr[i],arr.length)}
