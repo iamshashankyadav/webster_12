bfeeb3f5f4402d0bec76b7eddcbb683bf190afcb
iamshashankyadav <shashanky@iitbhilai.ac.in>
2026-03-16 20:14:16 +0530

    fixed childe nodes shit

--- javascript/dark.js
+++ javascript/dark.js
@@ -1,4 +1,16 @@
-divi=document.querySelector("#divi")
-divi.eventListener("onclick",()=>{
-    divi.style.backgroundColor="black"
+// let divi=document.querySelector(".divi")
+// console.dir(divi)
+// divi.eventListener("onclick",()=>{
+//     divi.style.backgroundColor="black"
+// })
+let divi=document.querySelector(".divi")
+console.dir(divi.childNodes[0])
+let button=divi.childNodes[0]
+button.addEventListener("click",()=>{
+    console.log("you arechzing mode")
+    if (document.body.style.backgroundColor=="black"){
+        document.body.style.backgroundColor="white"
+    }    else{document.body.style.backgroundColor="black"}
+    console.log(divi.style.backgroundColor)
+    
 })
