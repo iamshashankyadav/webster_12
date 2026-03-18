ab2febbca6090e8158f3a817f46aaaf0f9389baf
iamshashankyadav <shashanky@iitbhilai.ac.in>
2026-03-18 16:15:32 +0530

    added logic

--- javascript/callback_hell.js
+++ javascript/callback_hell.js
@@ -2,13 +2,17 @@ function getdata(a, callback) {
     setTimeout(() => {
         console.log("data received"+a);
         if (callback){
-            callback(a);
+            callback();
         }
 
             
     }, 2000);  
 }
 
-getdata(1);
-getdata(2);
-getdata(3); 
+// getdata(1);
+// getdata(2);
+// getdata(3); 
+getdata(1, () => {
+    getdata(2, () => {
+        getdata(3, () => {
+        })})})
