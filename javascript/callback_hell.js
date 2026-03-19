842fb933fa283c026863561251ab3f70f2f3f732
iamshashankyadav <shashanky@iitbhilai.ac.in>
2026-03-19 14:28:24 +0530

    promises

--- javascript/callback_hell.js
+++ javascript/callback_hell.js
@@ -1,18 +1,31 @@
-function getdata(a, callback) {
-    setTimeout(() => {
-        console.log("data received"+a);
-        if (callback){
-            callback();
-        }
+// function getdata(a, callback) {
+//     setTimeout(() => {
+//         console.log("data received"+a);
+//         if (callback){
+//             callback();
+//         }
 
             
-    }, 2000);  
-}
+//     }, 2000);  
+// }
 
-// getdata(1);
-// getdata(2);
-// getdata(3); 
-getdata(1, () => {
-    getdata(2, () => {
-        getdata(3, () => {
-        })})})
+// // getdata(1);
+// // getdata(2);
+// // getdata(3); 
+// getdata(1, () => {
+//     getdata(2, () => {
+//         getdata(3, () => {
+//         })})})
+
+function getdata(a) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("data received"+a);
+            resolve();
+        }, 2000);
+    });
+}
+getdata(1)
+.then(() => getdata(2))
+.then(() => getdata(3))     
+.then(() => setTimeout(console.log("all data received"),1000))
