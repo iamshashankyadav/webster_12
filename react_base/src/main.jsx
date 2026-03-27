c8f609aebe2d2e6c9bd17823d0cc27712d721a00
iamshashankyadav <shashanky@iitbhilai.ac.in>
2026-03-27 13:21:20 +0530

    instal oackage through nom

--- react_base/src/main.jsx
+++ react_base/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+alert('you entering the real world of dev')
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
