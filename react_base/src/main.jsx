d0389d2a446008607ab19d189d5010bf0bd64acf
iamshashankyadav <shashanky@iitbhilai.ac.in>
2026-04-05 17:02:10 +0530

    all fixes

--- react_base/src/main.jsx
+++ react_base/src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-alert('you entering the real world of dev')
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
